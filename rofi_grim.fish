1b0839925b287b76b8f41a6a19f66fff71b3a7a8
serg <91432388+hiimsergey@users.noreply.github.com>
2023-08-12 15:11:18 +0200

    Add files via upload

---
+++ rofi_grim.fish
@@ -0,0 +1,31 @@
+#!/bin/fish
+
+set tmp_file (mktemp /tmp/screenshot_XXXXXX.jpg)
+grim -t jpeg -q 100 $tmp_file
+
+set file_name (echo -n "" | rofi -dmenu -p "Enter file name: ")
+
+if test -z $file_name
+    notify-send "No file name entered. Aborting."
+    exit 1
+end
+
+if test -f $HOME/pictures/$file_name.jpg
+    set choice (echo -e "replace\nkeep & ignore" | rofi -dmenu -p "File with the same name exists.")
+
+    switch $choice
+    case "replace"
+        mv "$tmp_file" "$HOME/pictures/$file_name.jpg"
+        notify-send "Screenshot replaced the existing picture."
+    case "keep & ignore"
+        rm $tmp_file
+        notify-send "Screenshot discarded. Existing picture kept."
+    case "*"
+        notify-send "Invalid choice of no action taken. Aborting."
+        rm $tmp_file
+        exit 1
+    end
+else
+    mv "$tmp_file" "$HOME/pictures/$file_name.jpg"
+    notify-send "Screenshot saved as $file_name.jpg"
+end
