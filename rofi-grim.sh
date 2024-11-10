730e7cb0920e51383432a5722a9160a5dab5f0eb
Sergey Lavrent <91432388+hiimsergey@users.noreply.github.com>
2024-11-10 17:22:03 +0100

    just comments...

--- rofi-grim.sh
+++ rofi-grim.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-# Wraps grim (Wayland screenshot utility) around a usable rofi prompt
+# Use grim (Wayland screenshot utility) in a Rofi prompt
 
 canberra-gtk-play -i screen-capture &
 
 # Take a screenshot using grim command and save it with a temporary file name
 tmp_file=$(mktemp /tmp/grim_XXXXXX.png)
-grim -t png -q 100 $tmp_file
+grim -t jpeg -q 100 $tmp_file
 
 while true; do
     # Use Rofi prompt to ask for a file name
     file_name=$(echo -n "" | rofi -dmenu -p "Enter file name:")
     
     # Check if a file name was entered
-    if [ -z "$file_name" ]; then
+    if [[ -z "$file_name" ]]; then
         notify-send "No file name entered. Aborting."
         exit 1
     fi
     
-    if [ -f "$HOME/pictures/$file_name.png" ]; then
+    if [[ -f $HOME/pictures/$file_name.jpg ]]; then
         choice=$(echo -e "rename\nreplace\nkeep & ignore" | rofi -dmenu -p "File with the same name exists!")
     
         case "$choice" in
@@ -25,24 +25,24 @@ while true; do
                 continue
                 ;;
             "replace")
-                mv "$tmp_file" "$HOME/pictures/$file_name.png"
+                mv "$tmp_file" "$HOME/pictures/$file_name.jpg"
                 notify-send "Screenshot replaced the existing picture."
                 break
                 ;;
             "keep & ignore")
-                rm "$tmp_file"
+                rm $tmp_file
                 notify-send "Screenshot discarded. Existing picture kept."
                 break
                 ;;
             *)
                 notify-send "Invalid choice or no action taken. Aborting."
-                rm "$tmp_file"
+                rm $tmp_file
                 exit 1
                 ;;
         esac
     else
-        mv "$tmp_file" "$HOME/pictures/$file_name.png"
-        notify-send "Screenshot saved as $file_name.png"
+        mv "$tmp_file" "$HOME/pictures/$file_name.jpg"
+        notify-send "Screenshot saved as $file_name.jpg"
         break
     fi
 done
--- rofi-wfr.sh
+++ rofi-wfr.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-# Wraps wf-recorder (Wayland screen recorder) around a usable Rofi prompt
+# Use wf-recorder (Wayland screen recorder) in a Rofi prompt
 
 while true; do
     file_name=$(echo -n "" | rofi -dmenu -p "Enter file name")
 
-    if [ -z "$file_name" ]; then
+    if [[ -z "$file_name" ]]; then
         notify-send "No file name entered. Aborting."
         exit 1
     fi
 
-    if [ -f "$HOME/videos/$file_name.mp4" ]; then
+    if [[ -f $HOME/videos/$file_name.mp4 ]]; then
         choice=$(echo -e "rename\nreplace\nkeep & ignore" | rofi -dmenu -p "File with the same name exists!")
 
         case "$choice" in
